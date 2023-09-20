fc87637674b70b258ba73213bbf938dc5dad5294
zilae <2081531631@qq.com>
2023-09-20 11:06:37 +0800

    modify test script

--- scripts/task/test_ngp.sh
+++ scripts/task/test_ngp.sh
@@ -3,7 +3,8 @@
 EXP_NAME="supervise_pixel"
 NETWORK="ngp"
 TRAIN_NUM=8
-DATA_BASE="data/nerf_llff_data"
+DATASET="nerf_llff_data"
+DATA_BASE="data/${DATASET}"
 TEST_SCENE="room"
 DOWNSCALE=4
 RESUME=0
@@ -14,10 +15,10 @@ RESUME=0
 python scripts/llff2nerf.py ${DATA_BASE}/${TEST_SCENE} --images images_${DOWNSCALE} --downscale ${DOWNSCALE} --train_num ${TRAIN_NUM}
 
 if [ ${RESUME} -eq 0 ]; then  
-    rm -r ./log/spaseview/${EXP_NAME}/${DATA_BASE}/${TEST_SCENE}/view_${TRAIN_NUM}/${NETWORK}
+    rm -r ./log/spaseview/${EXP_NAME}/${DATASET}/${TEST_SCENE}/view_${TRAIN_NUM}/${NETWORK}
 fi
 
-python main_nerf.py ${DATA_BASE}/${TEST_SCENE} --workspace ./log/spaseview/${EXP_NAME}/${DATA_BASE}/${TEST_SCENE}/view_${TRAIN_NUM}/${NETWORK} -O --iters 2500 --num_rays 4096
+python main_nerf.py ${DATA_BASE}/${TEST_SCENE} --workspace ./log/spaseview/${EXP_NAME}/${DATASET}/${TEST_SCENE}/view_${TRAIN_NUM}/${NETWORK} -O --iters 2500 --num_rays 4096
 
 # 测试多场景
 
@@ -29,7 +30,7 @@ python main_nerf.py ${DATA_BASE}/${TEST_SCENE} --workspace ./log/spaseview/${EXP
 # do  
 #     python scripts/llff2nerf.py ${DATA_BASE}/$(basename $file) --images images_${DOWNSCALE} --downscale ${DOWNSCALE} --train_num ${TRAIN_NUM}
 #     if [ ${RESUME} -eq 0 ]; then  
-#         rm -r ./log/spaseview/${EXP_NAME}/${DATA_BASE}/$(basename $file)/view_${TRAIN_NUM}/${NETWORK}
+#         rm -r ./log/spaseview/${EXP_NAME}/${DATA_SET}/$(basename $file)/view_${TRAIN_NUM}/${NETWORK}
 #     fi
-#     python main_nerf.py ${DATA_BASE}/${TEST_SCENE} --workspace ./log/spaseview/${EXP_NAME}/${DATA_BASE}/$(basename $file)/view_${TRAIN_NUM}/${NETWORK} -O --iters 2500 --num_rays 4096
+#     python main_nerf.py ${DATA_BASE}/${TEST_SCENE} --workspace ./log/spaseview/${EXP_NAME}/${DATASET}/$(basename $file)/view_${TRAIN_NUM}/${NETWORK} -O --iters 2500 --num_rays 4096
 # done  
